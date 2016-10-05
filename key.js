1ae927259e244b44c192a066cc9d801b20629fa7
Yuuta Sumioka <ysumio120@yahoo.com>
2016-10-05 16:50:37 -0400

    Added all necessary files

---
+++ key.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'swLSrNGLYgdXPkrDTZBpD5VST',
+  consumer_secret: 'DHmh6TgR8z8ATYSMNrFNmHllPs2gQpi6N3fdZVo7qNb2EPy7Hp',
+  access_token_key: '779105290121842689-MgqquxNyj0LrnJEGVPsKhlJ38eKBgSm',
+  access_token_secret: 'DEt1U0a3PS4xNDRjnL8g0JkJQczixQ8L07DEC6TszTaX1',
+}
